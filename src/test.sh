ffbab1d1cd8f119303e74b33b82bd59424f6da92
Thomas <thomas.mortier92@gmail.com>
2023-11-20 11:33:09 +0000

    weighted loss script

--- src/test.sh
+++ src/test.sh
@@ -5,5 +5,9 @@
 #python test.py -m conv -d test_sequences.csv -mp model_dms_1_2310_457.ckpt -embs 128 -nl 10 -seqlen 457 -bs 512 -dev 1 --out submission_DMS_1_2310_457
 
 # MTM model on high-quality sequences that occur in both DMS and 2A3 experiments
-python test.py -m conv -d test_sequences.csv -mp model_2A3_1N_1811_457.ckpt -embs 128 -nl 10 -seqlen 457 -bs 512 -dev 1 --out submission_2A3_1N_1811_457
-python test.py -m conv -d test_sequences.csv -mp model_dms_1N_1811_457.ckpt -embs 128 -nl 10 -seqlen 457 -bs 512 -dev 1 --out submission_DMS_1N_1811_457
+#python test.py -m conv -d test_sequences.csv -mp model_2A3_1N_1811_457.ckpt -embs 128 -nl 10 -seqlen 457 -bs 512 -dev 1 --out submission_2A3_1N_1811_457
+#python test.py -m conv -d test_sequences.csv -mp model_dms_1N_1811_457.ckpt -embs 128 -nl 10 -seqlen 457 -bs 512 -dev 1 --out submission_DMS_1N_1811_457
+
+# MTM model on high-quality sequences that occur in both DMS and 2A3 experiments with weighted loss
+python test.py -m conv -d test_sequences.csv -mp model_2A3_1N_w_1911_457.ckpt -embs 128 -nl 10 -seqlen 457 -bs 512 -dev 1 --out submission_2A3_1N_w_1911_457
+python test.py -m conv -d test_sequences.csv -mp model_dms_1N_w_1911_457.ckpt -embs 128 -nl 10 -seqlen 457 -bs 512 -dev 1 --out submission_DMS_1N_w_1911_457
